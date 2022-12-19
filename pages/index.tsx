c5f6c063cab8ab5c351652803399bcb8629ae1f3
AudioStakes <daisuke.sato@enishi-tech.com>
2022-12-19 11:47:36 +0900

    Try to change content from main

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World!
+          Hello World! from non-main branch
         </h1>
 
         <p className={styles.description}>
